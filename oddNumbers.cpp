6ea7e16b9c95940d2222164042d0d3600bb97e37
grantmwilliams <grant.williams@okstate.edu>
2015-06-16 01:55:51 -0500

    Added oddNumbers in C++

---
+++ oddNumbers.cpp
@@ -0,0 +1,13 @@
+//Displays the odd numbers one per line.
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	for (short i = 1; i < 100; i+=2)
+		std::cout<< i << std::endl;
+
+	return 0;
+}
