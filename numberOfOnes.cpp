b692c56c75e9b730b0a50ff9f0f0bbc8974f8524
Grant Williams <grant.williams@okstate.edu>
2015-06-18 20:13:54 -0500

    added comments to number of ones C++

--- numberOfOnes.cpp
+++ numberOfOnes.cpp
@@ -6,12 +6,13 @@ using namespace std;
 void number_of_ones(int val)
 {
 	int total = 0;
+	
 	while (val)
 	{
-		if (val & 1)
+		if (val & 1) //bitwise AND operator
 			total++;
-		val >>= 1;
-	}
+		val >>= 1; // Shift bits right
+	}	
 	std::cout << total << std::endl;
 }
 
